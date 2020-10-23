69673fa3d57b6e73ba58b68619166d400cafcedd
umbur <work1415@gmail.com>
2020-10-22 21:23:15 -0600

    adds state to SearchBar

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import logo from "../../logo.svg";
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 
