785c82bc41e3c5358b04572347d3cdb68500d5b9
umbur <work1415@gmail.com>
2020-10-22 23:06:34 -0600

    adds test functionality to search by location and restaurant, completes part 3

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -10,6 +10,9 @@ class SearchBar extends React.Component {
       "Highest Rated": "rating",
       "Most Reviewed": "review_count",
     };
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
   renderSortByOptions() {
     return Object.keys(this.sortByOptions).map((sortByOption) => {
@@ -35,6 +38,21 @@ class SearchBar extends React.Component {
   handleSortByChange(sortByOption) {
     this.setState({ sortBy: sortByOption });
   }
+  handleTermChange(e) {
+    this.setState({ term: e.target.value });
+  }
+  handleLocationChange(e) {
+    this.setState({});
+    this.setState({ location: e.target.value });
+  }
+  handleSearch(e) {
+    this.props.searchYelp(
+      this.state.term,
+      this.state.location,
+      this.state.sortBy
+    );
+    e.preventDefault();
+  }
   render() {
     return (
       <div className="SearchBar">
@@ -42,11 +60,14 @@ class SearchBar extends React.Component {
           <ul>{this.renderSortByOptions()}</ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input
+            onChange={this.handleTermChange}
+            placeholder="Search Businesses"
+          />
+          <input onChange={this.handleLocationChange} placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
-          <a>Let's Go</a>
+          <a onClick={this.handleSearch}>Let's Go</a>
         </div>
       </div>
     );
