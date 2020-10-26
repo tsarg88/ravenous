0f1b01cae315388163c344703ff0deac1bf41665
umbur <work1415@gmail.com>
2020-10-26 17:27:36 -0600

    completes searchYelp method to fetch data from yelp api

--- ravenous/src/util/Yelp.js
+++ ravenous/src/util/Yelp.js
@@ -1,2 +1,41 @@
 const apiKey =
   "7wznfCvfrj7UHkRtk5xkw8UDU6JPkAKTKoIRyGsjM-uT8A4yehJE5QwdR40TnIdC7kZZwGUdIKrTitGfuwa2QsqdoakmhYtOOWydO3nme0zisUzRSXPFkQMUGE6XX3Yx";
+
+const Yelp = {
+  searchYelp(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/
+
+          https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.business) {
+          return jsonResponse.businesses.map((business) => {
+            console.log(business);
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.review_count,
+              reviewCount: business.review_count,
+            };
+          });
+        }
+      });
+  },
+};
+
+export default Yelp;
